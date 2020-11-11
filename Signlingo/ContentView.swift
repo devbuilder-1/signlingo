66788ae97259d7a248d583a087f684b6acdb401e
Sanjay LNU <sanjay@usecamo.com>
2020-11-11 17:49:03 -0500

    Initial Commit

---
+++ Signlingo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Signlingo
+//
+//  Created by Prithiv Dev on 11/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Signlingo/SignlingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignlingoApp.swift
+//  Signlingo
+//
+//  Created by Prithiv Dev on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SignlingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
